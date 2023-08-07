cb777795eefbcb3d27f94e1032529c117a2459f9
Arslan Majeed <arslanmajeed214@gmail.com>
2023-08-08 01:08:54 +0500

    Implement the functionality of add and remove books in the local storage.

--- script.js
+++ script.js
@@ -1,18 +1,40 @@
-const titleInput = document.getElementById('title');
-const authorInput = document.getElementById('author');
-const addBtn = document.getElementById('addBtn');
-const bookList = document.getElementById('booksList');
-let books = JSON.parse(localStorage.getItem('books'));
+const titleInput = document.getElementById("title");
+const authorInput = document.getElementById("author");
+const addBtn = document.getElementById("addBtn");
+const bookList = document.getElementById("booksList");
+let books = JSON.parse(localStorage.getItem("books"));
 function showBooks() {
-    bookList.innerHTML = '';
-    books.forEach((book, index) => {
-        const bookContainer = document.createElement('div');
-        bookContainer.innerHTML = `
+  bookList.innerHTML = "";
+  books.forEach((book, index) => {
+    const bookContainer = document.createElement("div");
+    bookContainer.innerHTML = `
         <p>${book.title}</p>
         <p>${book.author}</p>
         <button onclick='removeBook(${index})'>Remove</button>
         <hr/>
         `;
-        bookList.appendChild(bookContainer);
-    });
-};
+    bookList.appendChild(bookContainer);
+  });
+}
+
+function addBook() {
+  const title = titleInput.value;
+  const author = authorInput.value;
+  if (title && author) {
+    books.push({ title, author });
+    localStorage.setItem("books", JSON.stringify(books));
+    showBooks();
+    titleInput.value = "";
+    authorInput.value = "";
+  }
+}
+
+function removeBook(index) {
+  books.splice(index, 1);
+  localStorage.setItem("books", JSON.stringify(books));
+  showBooks();
+}
+
+addBtn.addEventListener("click", addBook);
+
+showBooks();
