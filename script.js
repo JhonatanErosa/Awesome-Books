7056be5eea3ae5a83b083df7523200839e2c079b
Jhonatan <jhonatancaama@gmail.com>
2023-08-07 15:38:24 -0600

    Fix linters in the project

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
-const titleInput = document.getElementById("title");
-const authorInput = document.getElementById("author");
-const addBtn = document.getElementById("addBtn");
-const bookList = document.getElementById("booksList");
-let books = JSON.parse(localStorage.getItem("books"));
+const titleInput = document.getElementById('title');
+const authorInput = document.getElementById('author');
+const addBtn = document.getElementById('addBtn');
+const bookList = document.getElementById('booksList');
+const books = JSON.parse(localStorage.getItem('books'));
+
 function showBooks() {
-  bookList.innerHTML = "";
+  bookList.innerHTML = '';
   books.forEach((book, index) => {
-    const bookContainer = document.createElement("div");
+    const bookContainer = document.createElement('div');
     bookContainer.innerHTML = `
         <p>${book.title}</p>
         <p>${book.author}</p>
-        <button onclick='removeBook(${index})'>Remove</button>
+        <button class='removeButton' data-index='${index}'>Remove</button>
         <hr/>
         `;
     bookList.appendChild(bookContainer);
@@ -22,19 +23,26 @@ function addBook() {
   const author = authorInput.value;
   if (title && author) {
     books.push({ title, author });
-    localStorage.setItem("books", JSON.stringify(books));
+    localStorage.setItem('books', JSON.stringify(books));
     showBooks();
-    titleInput.value = "";
-    authorInput.value = "";
+    titleInput.value = '';
+    authorInput.value = '';
   }
 }
 
 function removeBook(index) {
   books.splice(index, 1);
-  localStorage.setItem("books", JSON.stringify(books));
+  localStorage.setItem('books', JSON.stringify(books));
   showBooks();
 }
 
-addBtn.addEventListener("click", addBook);
+bookList.addEventListener('click', (event) => {
+  if (event.target.classList.contains('removeButton')) {
+    const index = parseInt(event.target.dataset.index, 10);
+    removeBook(index);
+  }
+});
+
+addBtn.addEventListener('click', addBook);
 
 showBooks();
