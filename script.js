48d508db0e30548e2a29df7efef0cff1a25d02a4
umarahgul <umarah.gul@gmail.com>
2023-08-10 03:12:35 +0500

    Created class library ,added showBooks and functionality for local storage

--- script.js
+++ script.js
@@ -1,76 +1,57 @@
-const newArr = [
-  { title: 'Clean Code A Handbook of Agile Software Craftsmanship', author: 'Robert C. Martin' },
-  { title: 'JavaScript The Good Parts', author: 'Douglas Crockford' },
-];
-
-if (localStorage.getItem('Library') == null) {
-  localStorage.setItem('Library', JSON.stringify(newArr));
-}
-
-const library = JSON.parse(localStorage.getItem('Library'));
-
-function updatelocalStorage() {
-  localStorage.setItem('Library', JSON.stringify(library));
-}
-
-const title = document.getElementById('title');
-const author = document.getElementById('author');
-const addBtn = document.getElementById('addBtn');
-
-function createNewBooks(arr) {
-  let books = '';
-  for (let i = 0; i < arr.length; i += 1) {
-    books += `
-      <div>
-          <p>${arr[i].title}</p>
-          <p>${arr[i].author}</p>
-          <button class="removeBtn" data-index="${i}">Remove</button>
-          <hr/>
-      </div>
-      `;
+class Library {
+  constructor() {
+      this.books = this.getBooksFromLocalStorage() || this.getDefaultBooks();
   }
-  return books;
-}
-
-function showBooks() {
-  const booksContainer = document.querySelector('#booksContainer');
-  booksContainer.innerHTML = `
-      <div class='bookWrapper'>
-      ${createNewBooks(library)}</div>
-  `;
-}
-
-function addData(Title, Author) {
-  const Book = {
-    title: Title,
-    author: Author,
-  };
-  library.push(Book);
-  updatelocalStorage();
-  showBooks();
-}
-
-function removeBook(index) {
-  library.splice(index, 1);
-  updatelocalStorage();
-  showBooks();
-}
-
-addBtn.addEventListener('click', (e) => {
-  e.preventDefault();
-  addData(title.value, author.value);
-
-  title.value = '';
-  author.value = '';
-});
-
-document.addEventListener('click', (event) => {
-  if (event.target.classList.contains('removeBtn')) {
-    const index = parseInt(event.target.getAttribute('data-index'), 10);
-    if (!Number.isNaN(index)) {
-      removeBook(index);
-    }
+  getBooksFromLocalStorage() {
+      return JSON.parse(localStorage.getItem('Library'));
   }
-});
-
-window.onload = showBooks;
+  getDefaultBooks() {
+      return [
+          { title: 'Clean Code A Handbook of Agile Software Craftsmanship', author: 'Robert C. Martin' },
+          { title: 'JavaScript The Good Parts', author: 'Douglas Crockford' },
+        ];
+  }
+  initializeLocalStorage() {
+      const storedData = this.getBooksFromLocalStorage();
+      if (storedData === null) {
+          localStorage.setItem('Library', JSON.stringify(this.getDefaultBooks()));
+      }
+  }
+  updateLocalStorage(){
+      localStorage.setItem('Library', JSON.stringify(this.books));
+  }
+  showBooks() {
+      const booksContainer = document.querySelector('#booksContainer');
+      booksContainer.innerHTML= '';
+      for(let i = 0; i < this.books.length; i += 1) {
+          const book = this.books[i];
+          const bookWrapper = document.createElement('div');
+          bookWrapper.classList.add('bookContent');
+          const bookInfo = document.createElement('div');
+          bookInfo.classList.add('bookInfo');
+          const bookTitle = document.createElement('p');
+          bookTitle.classList.add('bookTitle');
+          bookTitle.textContent = `${book.title}`;
+          const by = document.createElement('p');
+          by.classList.add('by');
+          by.textContent = 'by';
+          const bookAuthor = document.createElement('p');
+          bookAuthor.classList.add('bookAuthor');
+          bookAuthor.textContent = `${book.author}`;
+          const buttonContainer = document.createElement('div');
+          buttonContainer.classList.add('buttonContainer')
+          const removeBtn = document.createElement('button');
+          removeBtn.textContent = 'Remove';
+          removeBtn.classList.add('removeBtn');
+          removeBtn.dataset.index = i;
+          bookInfo.appendChild(bookTitle);
+          bookInfo.appendChild(by)
+          bookInfo.appendChild(bookAuthor);
+          buttonContainer.appendChild(removeBtn);
+          bookWrapper.appendChild(bookInfo)
+          bookWrapper.appendChild(buttonContainer);
+          booksContainer.appendChild(bookWrapper);
+          removeBtn.addEventListener('click', () => this.removeBook(i));
+      }
+  } 
+}
