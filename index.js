b70ea88573a2607dd6bf6a9916e87646fc34884e
Jhonatan <jhonatancaama@gmail.com>
2023-08-10 17:23:32 -0600

    Fix linters in JavaScript and CSS files

--- index.js
+++ index.js
@@ -1,49 +1,50 @@
-const list = document.querySelector("#List");
-const addNew = document.querySelector("#AddNew");
-const contact = document.querySelector("#Contact");
-const listSection = document.querySelector("#listSection");
-const addSection = document.querySelector("#addSection");
-const contactSection = document.querySelector("#contactSection");
+const list = document.querySelector('#List');
+const addNew = document.querySelector('#AddNew');
+const contact = document.querySelector('#Contact');
+const listSection = document.querySelector('#listSection');
+const addSection = document.querySelector('#addSection');
+const contactSection = document.querySelector('#contactSection');
 
-list.addEventListener("click", () => {
-  listSection.classList.replace("listSectionHide", "listSection");
-  addSection.classList.replace("addSection", "addSectionHide");
-  contactSection.classList.replace("contactSection", "contactSectionHide");
+list.addEventListener('click', () => {
+  listSection.classList.replace('listSectionHide', 'listSection');
+  addSection.classList.replace('addSection', 'addSectionHide');
+  contactSection.classList.replace('contactSection', 'contactSectionHide');
 });
 
-addNew.addEventListener("click", () => {
-  listSection.classList.replace("listSection", "listSectionHide");
-  addSection.classList.replace("addSectionHide", "addSection");
-  contactSection.classList.replace("contactSection", "contactSectionHide");
+addNew.addEventListener('click', () => {
+  listSection.classList.replace('listSection', 'listSectionHide');
+  addSection.classList.replace('addSectionHide', 'addSection');
+  contactSection.classList.replace('contactSection', 'contactSectionHide');
 });
 
-contact.addEventListener("click", () => {
-  listSection.classList.replace("listSection", "listSectionHide");
-  addSection.classList.replace("addSection", "addSectionHide");
-  contactSection.classList.replace("contactSectionHide", "contactSection");
+contact.addEventListener('click', () => {
+  listSection.classList.replace('listSection', 'listSectionHide');
+  addSection.classList.replace('addSection', 'addSectionHide');
+  contactSection.classList.replace('contactSectionHide', 'contactSection');
 });
 
-const date = document.querySelector("#date");
+const date = document.querySelector('#date');
+
+function formatTime(value) {
+  return value < 10 ? `0${value}` : value;
+}
+
 function getCurrentDate() {
   const currentDate = new Date();
   const options = {
-    month: "long",
-    day: "numeric",
-    year: "numeric",
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric',
   };
   const hours = currentDate.getHours();
   const minutes = formatTime(currentDate.getMinutes());
   const seconds = formatTime(currentDate.getSeconds());
   const formatHours = formatTime(((hours + 11) % 12) + 1);
-  const format = hours < 12 || hours === 24 ? "AM" : "PM";
+  const format = hours < 12 || hours === 24 ? 'AM' : 'PM';
   date.innerHTML = `${currentDate.toLocaleDateString(
-    "en",
-    options
+    'en',
+    options,
   )} ${formatHours}:${minutes}:${seconds} <small>${format}</small>`;
-}
-
-function formatTime(value) {
-  return value < 10 ? `0${value}` : value;
 }
 
 getCurrentDate();
